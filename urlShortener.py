2ffe011e70231193fcaaba840551f57b008c134a
Afeefa Kauser <155752128+afeefakauser407@users.noreply.github.com>
2024-01-29 12:05:34 +0530

    Update urlShortener.py
    remove errors

--- urlShortener.py
+++ urlShortener.py
@@ -1,16 +1,18 @@
 import tkinter as tk
+from tkinter import Entry, Label, Button, END
 import pyshorteners
-from tkinter import *
 
 # Create the root window
-root = Tk()
+root = tk.Tk()
 root.geometry("500x300")
+root.title("URL Shortener")
 root.configure(bg="lightblue")
 
 # Function for URL shortening
 def shortener():
     url = entry.get()
-    url_short = pyshorteners.Shortener().tinyurl.short(url)
+    s = pyshorteners.Shortener()
+    url_short = s.tinyurl.short(url)
     entry1.delete(0, END)
     entry1.insert(END, url_short)
 
@@ -18,15 +20,15 @@ def shortener():
 Label(root, text="Enter the URL", font="arial 20 bold", bg="lightblue", fg="white").pack(fill='x')
 
 # Entry box for URL input
-entry = Entry(root, font="arial 14 ", bg="white", fg="lightblue", bd=3, width=40)
+entry = Entry(root, font="arial 14", bg="white", fg="lightblue", bd=3, width=40)
 entry.pack(pady=20)
 
 # Button to trigger the URL shortening function
 Button(root, text="Submit", font="arial 12 bold", bg="white", fg="lightblue", bd=3, command=shortener).pack()
 
 # Entry for displaying the shortened URL
-entry1 = Entry(root, font="arial 20 ", bg="lightblue", fg="white", width=40)
+entry1 = Entry(root, font="arial 20", bg="lightblue", fg="white", width=40)
 entry1.pack(pady=20)
 
 # Start the main loop
-mainloop()
+root.mainloop()
